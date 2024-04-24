01ce8ab39af5b1702b266818e5306982b02726de
Hiboux <autopreneurfastcar@gmail.com>
2024-04-24 15:40:06 +0200

    Initial Commit

---
+++ PriereSwuiftUI/PriereSwuiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PriereSwuiftUIApp.swift
+//  PriereSwuiftUI
+//
+//  Created by abbas on 24/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct PriereSwuiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
